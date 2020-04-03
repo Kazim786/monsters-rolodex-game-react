a1a02cd9bd31336f03782be38267c43492ba33dc
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-03 17:55:03 -0500

    imported the CSS

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import './card-list.styles.css'
 
 
 export const CardList = (props) => {
     console.log(props);
-return (<h1>{props.name}</h1>)}
+return (<div className= 'card-list'><h1>{props.name}</h1></div>)}
