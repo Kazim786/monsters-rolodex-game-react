9eb859d2e28ca45d9f49ae98a12c5a3556f5660d
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-04-04 12:52:27 -0500

    added filtered feature to it

--- src/App.js
+++ src/App.js
@@ -20,11 +20,16 @@ componentDidMount(){
   
 }
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
     return(
 
           <div className="App">
-            <input type= 'search' placeholder= "Search Monsters" onChange = {e => console.log(e.target)} />
-            <CardList monsters={this.state.monsters} > 
+            <input type= 'search' placeholder= "Search Monsters" 
+            onChange= {e => this.setState({searchField: e.target.value}, () => console.log(this.state))} /> <br></br>
+            <CardList monsters={filteredMonsters} > 
             {
             
             }
